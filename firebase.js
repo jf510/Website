77b2cea597c9d6b7cb6a696c260339859ba144e1
Jfontaine <jfontaine19@gmail.com>
2022-07-02 20:02:50 -0400

    initial commit

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBeBRjXdov_A1kjg-rfI8tlPfXpj2pYhpM",
+  authDomain: "jordan-fontaine.firebaseapp.com",
+  projectId: "jordan-fontaine",
+  storageBucket: "jordan-fontaine.appspot.com",
+  messagingSenderId: "741663754476",
+  appId: "1:741663754476:web:6ab94b84f4b42ceb587dc9",
+  measurementId: "G-8WNJKPBTY7",
+};
